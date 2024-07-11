cf49ba33175a0de71d735a80845766abbb091740
lhk228 <75469745+lhk228@users.noreply.github.com>
2024-07-12 02:38:25 +0900

    -

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,15 +1,68 @@
+function debounceOneFrame(fn) {
+    let currentRequestIndex = -1;
+
+    return (...args) => {
+        cancelAnimationFrame(currentRequestIndex);
+        currentRequestIndex = requestAnimationFrame(() => fn(...args));
+    };
+}
+
 export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
-  };
+    const debounceCallback = debounceOneFrame(callback);
+    const stateContext = {
+        current: 0,
+        states: [],
+    };
 
-  const useMemo = (fn, refs) => {
-    return fn();
-  };
+    const memoContext = {
+        current: 0,
+        memos: [],
+    };
 
-  const resetContext = () => {
+    function resetContext() {
+        stateContext.current = 0;
+        memoContext.current = 0;
+    }
 
-  }
+    const useState = (initState) => {
+        const { current, states } = stateContext;
+        stateContext.current += 1;
 
-  return { useState, useMemo, resetContext };
+        states[current] = states[current] ?? initState;
+
+        const setState = (newState) => {
+            if (newState === states[current]) return;
+            states[current] = newState;
+            debounceCallback();
+        };
+
+        return [states[current], setState];
+    };
+
+    const useMemo = (fn, refs) => {
+        const { current, memos } = memoContext;
+        memoContext.current += 1;
+
+        const memo = memos[current];
+
+        const resetAndReturn = () => {
+            const value = fn();
+            memos[current] = {
+                value,
+                refs,
+            };
+            return value;
+        };
+
+        if (!memo) {
+            return resetAndReturn();
+        }
+
+        if (refs.length > 0 && memo.refs.find((v, k) => v !== refs[k])) {
+            return resetAndReturn();
+        }
+        return memo.value;
+    };
+
+    return { useState, useMemo, resetContext };
 }
