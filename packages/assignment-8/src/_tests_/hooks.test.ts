22ac9b3bc8ef16c98e31a17b7c5913e0865fa724
lhk228 <75469745+lhk228@users.noreply.github.com>
2024-08-08 00:49:25 +0900

    house

---
+++ packages/assignment-8/src/_tests_/hooks.test.ts
@@ -0,0 +1,10 @@
+//훅 테스트 작성
+import { describe, expect, test } from 'vitest';
+
+describe('단위테스트 : 반복일정 관리', () => {
+  describe('useSchedule 훅', () => {
+    test('일정생성, 수정시 반복 일정을 체크하여 UI가 표시된다 ', () => {
+      expect('').toBe('');
+    });
+  });
+});
